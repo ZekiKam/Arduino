edadfec73487b3c614da04fbd76af6bdecb90bd2
Zeki Kam <155576230+ZekiKam@users.noreply.github.com>
2024-05-30 00:31:57 +0100

    Update Push_button_switch.ino

--- Push_button_switch.ino
+++ Push_button_switch.ino
@@ -1,13 +1,15 @@
+/* When pressed button -> pin 4 LED on*/
+
 void setup() {
 pinMode(4, OUTPUT);
-pinMode(2, INPUT_PULLUP);
+pinMode(2, INPUT_PULLUP);   /*Set pin 2 as on-board switch-input*/
 }
 
 void loop() 
-{
+{   /*main opening bracket for loop*/
 
-if (digitalRead(2) == LOW)
-{
+if (digitalRead(2) == LOW)   /*When button is pressed, it connects pin 2 to the ground -> low voltage*/
+{      /*brackets around statement, no brackets around if/ elif/ else*/
 digitalWrite(4, HIGH);
 }
 
@@ -16,4 +18,4 @@ else
 digitalWrite(4, LOW);
 }
 
-}
+}    /*main closing bracket for loop*/
