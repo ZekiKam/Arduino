3eefeaafbbeae4c54f2b1ad7ab371f249d9a3eb1
Zeki Kam <155576230+ZekiKam@users.noreply.github.com>
2024-05-29 23:43:58 +0100

    Update Blinking_LED.ino

--- Blinking_LED.ino
+++ Blinking_LED.ino
@@ -3,6 +3,8 @@ void setup() {
 pinMode(9,OUTPUT);
 pinMode(8, OUTPUT);
 }
+/*Sets the mode of pin 13 to OUTPUT
+pin 13 can now only be used to send signals*/
 
 void loop() {
 digitalWrite(9, HIGH);
@@ -13,8 +15,9 @@ digitalWrite(8, HIGH);
 delay(500);
 digitalWrite(8, LOW);
 delay(500);
-
-
-  // put your main code here, to run repeatedly:
-
 }
+/*Sets pin 13 to high voltage -> LED on
+Pauses program for 1s with LED on
+Low voltage -> LED off
+LED stays off for 1s
+This creates a blinking effect which loops endlessly*/
