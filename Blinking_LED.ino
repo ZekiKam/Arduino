d0f0255c6753995c25b6ea65716c5e2b0c39db4c
Zeki Kam <155576230+ZekiKam@users.noreply.github.com>
2024-05-29 23:42:08 +0100

    Add files via upload

---
+++ Blinking_LED.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(9,OUTPUT);
+pinMode(8, OUTPUT);
+}
+
+void loop() {
+digitalWrite(9, HIGH);
+delay(500);
+digitalWrite(9, LOW);
+delay(500);
+digitalWrite(8, HIGH);
+delay(500);
+digitalWrite(8, LOW);
+delay(500);
+
+
+  // put your main code here, to run repeatedly:
+
+}
